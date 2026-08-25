ca5218b28d8f2396dc6154d61e2c7cd12528c00c
vimalsainfoji <gamerfact195@gmail.com>
2026-08-25 14:27:06 +0530

    logout controller for the backend

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -167,8 +167,31 @@ export const loginUser = async (req, res) => {
 };
 
 
+// export const logoutUser = async (req, res) => {
+//   try {
+//     return res.status(200).json({
+//       success: true,
+//       message: "Logout successful",
+//     });
+//   } catch (error) {
+//     console.error("Logout Error:", error);
+
+//     return res.status(500).json({
+//       success: false,
+//       message: "Internal server error",
+//     });
+//   }
+// };
+
 export const logoutUser = async (req, res) => {
   try {
+    res.clearCookie("token", {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+      path: "/",
+    });
+
     return res.status(200).json({
       success: true,
       message: "Logout successful",
